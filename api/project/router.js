cc12c74859c37fa95f8cb70c8f10ad7dbfd1cbf8
Victoria Blancas <vikki6688@gmail.com>
2023-10-16 18:33:24 -0400

    wip

--- api/project/router.js
+++ api/project/router.js
@@ -3,6 +3,15 @@ const router = require('express').Router()
 //model - add after setup
 const Projects = require('./model')
 
+// router.get('/', (req, res, next)=>{
+//     Projects.getProjectById()
+//     .then(resource => {
+//         res.status(200).json(resource)
+//     })
+//     .catch(next)
+// })
+
+//doesn't return project list when i have ids in here...
 router.get('/:project_id', (req, res, next)=>{
     Projects.getProjectById(req.params.project_id)
     .then(resource => {
